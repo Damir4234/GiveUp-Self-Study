d266cb64b38f8cd3a8ba8d6c03c49aebd5bf3736
damir <saskedir@mail.ru>
2024-04-25 16:31:09 +0300

    added all main models

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Course(models.Model):
+    """Модель курса"""
+    title = models.CharField(
+        max_length=255, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='Урок')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    """Модель урока - теоретический материал, относится к одному конкретному курсу"""
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, verbose_name='Курс')
+    title = models.CharField(
+        max_length=255, verbose_name='Название')
+    content = models.TextField(verbose_name='Материал урока', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
+
+
+class Answer(models.Model):
+    lesson = models.ForeignKey(
+        Lesson, on_delete=models.CASCADE, verbose_name='урок')
+    question = content = models.TextField(
+        verbose_name='Материал урока')
+    correct_answer = models.CharField(
+        max_length=150, verbose_name='Название')
--- user/models.py
+++ user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+from materials.models import Answer
+
+
+class User(AbstractUser):
+    pass
+
+
+class CompletedTasks(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='Урок')
+    answer = models.ForeignKey(
+        Answer, on_delete=models.CASCADE, verbose_name='Урок')
+    is_correct = models.BooleanField(default=False)
