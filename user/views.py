1fb58517cc77fc76dab5856c4e05b417f38d9948
damir <saskedir@mail.ru>
2024-04-30 19:21:52 +0300

    Разработка CRUD пользователя с авторизацией по почте. Верия не рабочая

--- user/views.py
+++ user/views.py
@@ -1,11 +1,11 @@
 
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, DetailView
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth import get_user_model
 from django.contrib.auth.tokens import default_token_generator
-from django.http import Http404
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.views.generic import View
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -16,7 +16,10 @@ from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from user.models import User
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.hashers import make_password
 
 
 class UserCreateView(CreateView):
@@ -27,6 +30,8 @@ class UserCreateView(CreateView):
     def form_valid(self, form):
         user = form.save(commit=False)
         user.is_active = False  # Устанавливаем is_active в False
+        # Хешируем пароль перед сохранением
+        user.set_password(form.cleaned_data['password'])
         user.save()
 
         # Отправляем электронное письмо с ссылкой для активации учетной записи
@@ -38,11 +43,11 @@ class UserCreateView(CreateView):
         current_site = get_current_site(self.request)
         mail_subject = 'Активация учетной записи'
 
-    # Генерируем токен и кодируем UID для активации учетной записи
+        # Генерируем токен и кодируем UID для активации учетной записи
         uid = urlsafe_base64_encode(force_bytes(user.pk))
         token = default_token_generator.make_token(user)
 
-    # Передаем uid и token в контекст шаблона
+        # Передаем uid и token в контекст шаблона
         message = render_to_string('user/activation_email.html', {
             'user': user,
             'domain': current_site.domain,
@@ -66,3 +71,40 @@ class UserActivateView(View):
             return redirect('materials:home')
         else:
             raise Http404
+
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            # Перенаправление на страницу профиля
+            return redirect('user:profile', pk=user.pk)
+        else:
+            # В случае неудачной аутентификации, добавим сообщение об ошибке
+            messages.error(
+                request, 'Неверный адрес электронной почты или пароль.')
+            return render(request, 'user/login.html')
+    # В случае GET запроса или если аутентификация не удалась, показать форму входа
+    return render(request, 'user/login.html')
+
+
+class UserDetailView(DetailView):
+    model = User
+
+    def get_object(self):
+        # Получаем объект пользователя или 404 ошибку, если пользователь не найден
+        user = get_object_or_404(User, pk=self.kwargs['pk'])
+        # Вывод информации о пользователе для отладки
+        print("User detail:", user)
+        return user
+
+
+class UserUpdateView(UpdateView):
+    model = User
+    fields = ['first_name', 'last_name']
+    template_name = 'user/user_update_form.html'
+    success_url = reverse_lazy('user:update', kwargs={
+                               'pk': self.object.pk})  # not working
